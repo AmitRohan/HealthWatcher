d14d688c7dd8499b89aea333204f4819291afa58
Amit Rohan <amit@kruzr.co>
2020-03-28 01:52:50 +0530

    Permission Checks Added

--- app/src/main/java/com/kruzr/preemptive/healthwatcher/Primary.java
+++ app/src/main/java/com/kruzr/preemptive/healthwatcher/Primary.java
@@ -1,17 +1,24 @@
 package com.kruzr.preemptive.healthwatcher;
 
+import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class Primary extends AppCompatActivity {
 
     private String user;
     private int p;
+    private final int MY_PERMISSIONS_REQUEST_READ_CAMERA = 866;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,11 +90,34 @@ public class Primary extends AppCompatActivity {
 
     public void startScannerActivity(int page,View v){
         p = page;
-        Intent i = new Intent(v.getContext(),StartVitalSigns.class);
-        i.putExtra("Usr", user);
-        i.putExtra("Page", page);
-        startActivity(i);
-        finish();
+        if (ContextCompat.checkSelfPermission(Primary.this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED) {
+            Intent i = new Intent(v.getContext(),StartVitalSigns.class);
+            i.putExtra("Usr", user);
+            i.putExtra("Page", page);
+            startActivity(i);
+            finish();
+        }else{
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(Primary.this,
+                    Manifest.permission.CAMERA)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(Primary.this,
+                        new String[]{Manifest.permission.CAMERA},
+                        MY_PERMISSIONS_REQUEST_READ_CAMERA);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+
+
     }
 
     @Override
@@ -108,5 +138,12 @@ public class Primary extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == MY_PERMISSIONS_REQUEST_READ_CAMERA && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(getBaseContext(),"You can now press again.",Toast.LENGTH_LONG).show();
+        }
+    }
 }
 
