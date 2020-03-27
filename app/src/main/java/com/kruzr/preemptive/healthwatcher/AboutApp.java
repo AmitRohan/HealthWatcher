6b3eaf0168427e8ed0b2dcc1475ce6a786fedde1
Amit Rohan <amit@kruzr.co>
2020-03-28 02:25:57 +0530

    Migrated to Android X

--- app/src/main/java/com/kruzr/preemptive/healthwatcher/AboutApp.java
+++ app/src/main/java/com/kruzr/preemptive/healthwatcher/AboutApp.java
@@ -2,7 +2,7 @@ package com.kruzr.preemptive.healthwatcher;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class AboutApp extends AppCompatActivity {
 
--- app/src/main/java/com/kruzr/preemptive/healthwatcher/VitalSignsProcess.java
+++ app/src/main/java/com/kruzr/preemptive/healthwatcher/VitalSignsProcess.java
@@ -6,7 +6,7 @@ import android.content.res.Configuration;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.os.PowerManager;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.widget.ProgressBar;
